cae262bb5bcb5cdf8fb7631149374de59e39545f
kanon chakma <kanonchakma@pop-os.localdomain>
2022-06-03 23:09:44 +0600

    inverted-half-pyramid

--- pattern/inverted-Half-pyramid.cpp
+++ pattern/inverted-Half-pyramid.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int main(){
-    int row, column;
-    cout<<"Enter the row and column"<<endl;
-    cin>>row>>column;
+    int row=10;
 
     for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < column; j++)
+        for (int j = i; j < row; j++)
         {
             cout<<"*"<<" ";
         }
