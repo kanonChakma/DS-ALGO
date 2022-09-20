04023cb4e1ae46f813f496b455aba896cd7c0500
kanon chakma <kanonchakma@pop-os.localdomain>
2022-09-20 20:21:05 +0600

    add reverse

---
+++ Basic/reverse.js
@@ -0,0 +1,17 @@
+function Reverse1(str){
+    if(str.length<2 || typeof str !== "string" || !str){
+        return "provide valid string";
+    }
+    let item = str.length - 1;
+    let convert = [];
+    for( i = item; i>=0; i--){
+      convert.push(str[i]);
+    }
+    return convert.join('');
+}
+
+function Reverse2(str){
+ return str.splict('').Reverse().join('');   
+}
+
+const reverse3 = str => [...str].reverse.join();
